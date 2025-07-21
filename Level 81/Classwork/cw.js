e9b2ae01f3bdf37f33a930be5b9219d41b08b000
nikaaxuco <sandroyes977@gmail.com>
2025-07-21 19:29:05 +0400

    second commit

---
+++ Level 81/Classwork/cw.js
@@ -0,0 +1,19 @@
+
+let car = {
+    brand: "BMW",
+    model: "M5",
+    color: "Black"
+};
+
+console.log("Properties:");
+console.log("brand");
+console.log("model"); 
+console.log("color");
+
+console.log("\nValues:");
+console.log(car.brand);
+console.log(car.model);
+console.log(car.color);
+
+console.log(`\nThis is a ${car.brand} brand, specifically ${car.model} model, and it's ${car.color} in color.`);
+
