0e9bee245ee59822a4947b0a6f799b9105a8543b
nikaaxuco <sandroyes977@gmail.com>
2025-08-07 14:40:33 +0400

    second commit

--- Level 81/Classwork/cw.js
+++ Level 81/Classwork/cw.js
@@ -10,10 +10,10 @@ console.log("brand");
 console.log("model"); 
 console.log("color");
 
-console.log("\nValues:");
+console.log("Values:");
 console.log(car.brand);
 console.log(car.model);
 console.log(car.color);
 
-console.log(`\nThis is a ${car.brand} brand, specifically ${car.model} model, and it's ${car.color} in color.`);
+console.log(`This is a ${car.brand} brand, specifically ${car.model} model, and it's ${car.color} in color.`);
 
